9b29c07892c93bd39dd228d772b40226ba8b58e0
Ivan Miller <therealigmiller@gmail.com>
2022-08-20 15:36:27 +0300

    feat: add button shared

--- rslang/src/app/shared/shared-button/shared-button.component.ts
+++ rslang/src/app/shared/shared-button/shared-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-shared-button',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shared-button.component.scss']
 })
 export class SharedButtonComponent implements OnInit {
+
+  @Input()
+  buttonTitle = 'New Button'
 
   constructor() { }
 
