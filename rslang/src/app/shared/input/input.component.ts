7a0655f0bdbc560670ab2231ebeb4bd2bb35c58b
Igor-Novitski <pro100nke@gmail.com>
2022-08-20 15:04:12 +0300

    feat: add input component

---
+++ rslang/src/app/shared/input/input.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { InputType } from '../../../types/index';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss']
+})
+export class InputComponent implements OnInit {
+
+  @Input() placeholder?: string;
+  @Input() type: InputType;
+  @Input() id: string;
+  @Output() editValue = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    if( this.placeholder === undefined) { this.placeholder = ''};
+  }
+  
+}
