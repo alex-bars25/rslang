19198a0340b292be1fc9492e072f8711b1a535f2
Ivan Miller <therealigmiller@gmail.com>
2022-08-22 12:37:23 +0300

    feat: rechange the path

--- rslang/src/app/pages/homepage/homepage.module.ts
+++ rslang/src/app/pages/homepage/homepage.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage.component';
 import { LogoModule } from './components/logo/logo.module';
-import {BtnActionModule} from "./components/education-block/btn-action/btn-action.module";
+import {BtnActionModule} from "../../shared/btn-action/btn-action.module";
 
 
 @NgModule({
