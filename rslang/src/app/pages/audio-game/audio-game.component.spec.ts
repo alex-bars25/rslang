bd2ea76e3c2e3aae48f23e48193405ba42e143a2
Ivan Miller <therealigmiller@gmail.com>
2022-08-28 19:18:34 +0300

    feat: add game component

---
+++ rslang/src/app/pages/audio-game/audio-game.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AudioGameComponent } from './audio-game.component';
+
+describe('AudioGameComponent', () => {
+  let component: AudioGameComponent;
+  let fixture: ComponentFixture<AudioGameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AudioGameComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AudioGameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
