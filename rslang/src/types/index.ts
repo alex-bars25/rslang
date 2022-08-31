444cf9ec5f9309dc5703161af9a8140650df4aa5
alex-bars25 <alex.bars25@gmail.com>
2022-09-01 00:56:36 +0300

    feat: add registration and login

--- rslang/src/types/index.ts
+++ rslang/src/types/index.ts
@@ -1,1 +1,15 @@
 export type InputType = 'password' | 'text' | 'email';
+
+export interface User {
+  name?: string;
+  email: string;
+  password: string;
+}
+
+export interface LoggedUser {
+  message: string;
+  token: string;
+  refreshToken: string;
+  userId: string;
+  name: string;
+}
