0af7d7b2e181fddbe74ca809fc5ab6053ff41107
Igor-Novitski <pro100nke@gmail.com>
2022-09-03 21:56:16 +0300

    feat: add some api methods and words tagging

--- rslang/src/app/pages/textbook/textbook.component.ts
+++ rslang/src/app/pages/textbook/textbook.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IWord } from '../../../types/index';
+import { IWord, userWord } from '../../../types/index';
 import { ApiService } from 'src/app/services/api.service';
 
 @Component({
@@ -11,6 +11,7 @@ export class TextbookComponent implements OnInit {
 
   constructor(private api: ApiService) {}
 
+  userId: string = localStorage.getItem('userId')!;
   group: number = 0;
   page: number = 1;
   shadow: object =  {'box-shadow': 'inset 0 0 100px rgb(154, 243, 207)'};
@@ -35,6 +36,25 @@ export class TextbookComponent implements OnInit {
 
   updateWordsList: () => void  = (): void => {
     this.api.getWords(this.group, this.page -1).subscribe((words: IWord[]) => this.words = words)
+    }
+
+    
+    createDiffWord: (wordId: string) => void  = (wordId): void => {
+      this.api.createUserWord(this.userId, wordId, 
+      { "difficulty": "true", "optional": {}}
+      ).subscribe((word: object) => console.log(word));
+    }
+
+    createStudWord: (wordId: string) => void  = (wordId): void => {
+      this.api.createUserWord(this.userId, wordId, 
+      { "difficulty": "false", "optional": {}}
+      ).subscribe((word: object) => console.log(word));
+    }
+
+    updateToStudWord: (wordId: string) => void  = (wordId): void => {
+      this.api.updateUserWord(this.userId, wordId, 
+      { "difficulty": "false", "optional": {}}
+      ).subscribe((word: object) => console.log(word));
     }
 
   ngOnInit(): void {
